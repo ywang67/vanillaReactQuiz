b93e9b82afd71173181aac284388900f0b2fdc0c
Your Name <you@example.com>
2017-09-08 15:59:18 -0400

    forfun123

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,89 @@
+var quizQuestions = [
+  {
+      question: "Which game you like?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "Halo"
+          },
+          {
+              type: "Nintendo",
+              content: "Mario"
+          },
+          {
+              type: "Sony",
+              content: "Final Fantasy"
+          }
+      ]
+  },
+  {
+      question: "Which is your favoriate consle?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "X-Box360"
+          },
+          {
+              type: "Nintendo",
+              content: "Switch"
+          },
+          {
+              type: "Sony",
+              content: "Playstation 4"
+          }
+      ]
+  },
+  {
+      question: "Which of ACT game you like most?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "Hola"
+          },
+          {
+              type: "Nintendo",
+              content: "Shooting hero"
+          },
+          {
+              type: "Sony",
+              content: "quality"
+          }
+      ]
+  },
+  {
+      question: "Which of these games do you think is best?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "BioShock"
+          },
+          {
+              type: "Nintendo",
+              content: "The Legend of Zelda: Ocarina of Time"
+          },
+          {
+              type: "Sony",
+              content: "Final Fantasy VII"
+          }
+      ]
+  },
+  {
+      question: "What console would you prefer to own?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "X-Box One"
+          },
+          {
+              type: "Nintendo",
+              content: "Switch"
+          },
+          {
+              type: "Sony",
+              content: "Playstation 4 plus"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
